c68c13da6c2187078a456aa156803b9bd5d25d60
rngustj9139 <mnb9139@daum.net>
2022-03-21 18:58:32 +0900

    cp_utils and some 6MnistClassificationWithCnn.py

--- 6MnistClassificationWithCnn.py
+++ 6MnistClassificationWithCnn.py
@@ -2,11 +2,92 @@ import os
 
 import tensorflow as tf
 
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Activation
 
 from keras.losses import SparseCategoricalCrossentropy
 from keras import optimizers # optimizers.SGD
 from keras.metrics import Mean, SparseCategoricalAccuracy
+
+from utils.learning_env_setting1 import dir_setting
+from utils.dataset_utils import load_processing_mnist
+from utils.cp_utils import save_metrics_model, metric_visualizer
+from utils.basic_utils import resetter, training_reporter
+
+# === Hyperparameter Setting start ===
+CONTINUE_FLAG = False
+dir_name = 'exp1'
+start_epoch = 0
+
+train_ratio = 0.8
+train_batch_size, test_batch_size = 32, 128
+
+epoch = 30
+learning_rate = 0.01
+# === Hyperparameter Setting end ===
+
+path_dict = dir_setting(dir_name, CONTINUE_FLAG)
+train_ds, validation_ds, test_ds = load_processing_mnist(train_ratio, train_batch_size, test_batch_size)
+losses_accs = {
+    'train_losses':[],
+    'train_accs':[],
+    'validation_losses':[],
+    'validation_accs':[]
+}
+
+# === Model Implementation start ===
+# model = Sequential()
+# model.add(Conv2D(filters=8, kernel_size=(5, 5), padding='same', activation='relu'))
+# model.add(MaxPooling2D())
+class MnistClassifier(Model):
+    def __init__(self):
+        super(MnistClassifier, self).__init__()
+
+        self.conv1 = Conv2D(filters=8, kernel_size=(5, 5), padding='same', activation='relu')
+        # input 이미지는 28 x 28이다. 따라서 ((28 - 5)/1) + 1 => 24가 output의 크기이다. but same padding(stride없음)이므로 28이 output의 크기이다. [ ((n - k)/s) + 1 ]
+        self.conv1_pool = MaxPooling2D(pool_size=2, strides=2)
+        self.conv2 = Conv2D(filters=32, kernel_size=5, padding='same', activation='relu')
+        self.conv2_pool = MaxPooling2D(pool_size=2, strides=2)
+
+        self.flatten = Flatten()
+        self.dense1 = Dense(units=64, activation='relu')
+        self.dense2 = Dense(units=10, activation='softmax')
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.conv1_pool(x)
+        x = self.conv2(x)
+        x = self.conv2_pool(x)
+
+        x = self.flatten(x)
+        x = self.dense1(x)
+        x = self.dense2(x)
+
+        return x
+# === Model Implementation end ===
+
+loss_objects = SparseCategoricalCrossentropy
+optimizer = optimizers.SGD(learning_rate=learning_rate)
+
+train_loss = Mean()
+train_acc = SparseCategoricalAccuracy()
+
+validation_loss = Mean()
+validation_acc = SparseCategoricalAccuracy()
+
+test_loss = Mean()
+test_acc = SparseCategoricalAccuracy()
+
+@tf.function
+def trainer():
+    global train_ds, loss_objects, optimizer
+    global train_loss, train_acc
+
+    for images, labels in train_ds:
+        with tf.GradientTape() as tape: # forward propagation 진행
+            predictions = model(images)
+            loss = loss_objects(labels, predictions)
+
+        gradients = tape.gradient(loss, model.trainable_variables)
 
---
+++ utils/cp_utils.py
@@ -0,0 +1,69 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+def save_metrics_model(epoch, model, losses_accs, path_dict, save_period):
+    if epoch % save_period == 0:
+        model.save(os.path.join(path_dict['model_path'], 'epoch_' + str(epoch)))
+
+    np.savez_compressed(os.path.join(path_dict['cp_path'], 'losses_accs'),
+                        train_losses=losses_accs['train_losses'],
+                        train_accs=losses_accs['train_accs'],
+                        validation_losses=losses_accs['validation_losses'],
+                        validation_accs=losses_accs['validation_accs'])
+
+losses_accs = np.load('./C:/Users/mnb91/PycharmProjects/cnnstudy/losses_accs.npz')
+
+def metric_visualizer(losses_accs, save_path): # 각 epoch마다 저장
+    fig, ax = plt.subplots(figsize=(35, 15))
+    ax2 = ax.twinx()
+
+    epoch_range = np.arange(1, 1+len(losses_accs['train_losses']))
+
+    ax.plot(epoch_range, losses_accs['train_losses'], color='tab:blue', linewidth=2, label='Train Loss')
+    ax.plot(epoch_range, losses_accs['validation_losses'], color='tab:blue', linestyle=':', linewidth=2, label='Validation Loss')
+    ax2.plot(epoch_range, losses_accs['train_accs'], color='tab:orange', linewidth=2, label='Train Accuracy')
+    ax2.plot(epoch_range, losses_accs['validation_accs'], color='tab:orange', linestyle=':', linewidth=2, label='Validation Accuracy')
+
+    ax.legend(bbox_to_anchor=(1, 0.5), loc='upper right', fontsize=20, frameon=False)
+    ax2.legend(bbox_to_anchor=(1, 0.5), loc='lower right', fontsize=20, frameon=False)
+
+    ax_yticks = ax.get_yticks()
+    ax2_yticks = ax2.get_yticks()
+
+    ax_yticks_m, ax_yticks_M = ax_yticks[0], ax_yticks[-1]
+
+    ax_yticks = np.linspace(0, ax_yticks_M, 7)
+    ax2_yticks = np.arange(20, 101, 5)
+    ax2_yticks_minor = np.arange(20, 101, 1)
+
+    ax.set_yricks(ax_yticks)
+    ax.set_ylim([0, ax_yticks_M])
+    ax.set_yrickslabels(np.around(ax_yticks, 2))
+
+    ax2_ylim = ax2.get_ylim()
+    ax2.set_ylim([20, 100])
+    ax2.set_yticks(ax2_yticks)
+    ax2.set_yticks(ax2_yticks_minor, minor=True)
+
+    epoch_ticks = np.linspace(1, len(losses_accs['train_losses']), 10).astype(np.int)
+
+    ax.tick_params(labelsize=20, colors='tab:blue')
+    ax2.tick_params(labelsize=20, colors='tab:orange')
+    ax2.tick_params(which='minor', right=False)
+
+    ax.set_xticks(epoch_ticks)
+    ax2.set_xticks(epoch_ticks)
+    ax.set_xticklabels(epoch_ticks, color='k')
+
+    ax2.grid(axis='y')
+    ax2.grid(which='minor', linestyle=':')
+
+    ax.set_xlim(1, len(losses_accs['train_losses']))
+    ax2.set_xlim(1, len(losses_accs['train_losses']))
+
+    ax.set_ylabel('Cross Entropy Loss', fontsize=30, color='tab:blue')
+    ax2.set_ylabel('Accuracy', fontsize=30, color='tab:orange')
+
+    fig.savefig(save_path + '/losses_accs_visualization.png')
+    plt.close()
